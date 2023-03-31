b60f49b2f60938d478477d4f952a694beb557565
shellyscheng <shellysc13@gmail.com>
2023-03-30 20:41:08 -0700

    refactor: remove hardcoded squares

--- src/App.js
+++ src/App.js
@@ -23,24 +23,22 @@ export function Board({ xIsNext, squares, onPlay }) {
   const winner = calculateWinner(squares);
   let status = winner ? "Winner: " + winner : "Next player: " + (xIsNext ? "X" : "O")
 
+  const boardSquares =  []
+  for (let i = 0; i < 3; i++) {
+    const row = [];
+    for (let j = 0; j < 3; j++) {
+      const index = i * 3 + j
+      row.push(
+        <Square key={index} value={squares[index]} onSquareClick={() => handleClick(index)} />
+      )
+    }
+    boardSquares.push(<div className="board-row">{row}</div>)
+  }
+
   return (
     <div>
       <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
-      </div>
+      {boardSquares}
     </div>
   );
 }
@@ -58,21 +56,17 @@ export default function Game() {
     setCurrentMove(nextHistory.length - 1)
   }
 
-  function jumpTo(nextMove) {
-    setCurrentMove(nextMove)
-  }
-
   const moves = history.map((squares, move) => {
     let description;
     if (move > 0) {
-      description = "Go to move # " + move;
+      description = "Go to move #" + move;
     } else {
       description = "Go to game start"
     }
 
     return (
       <li key={move}>
-        <button onClick={() => jumpTo(move)}>{description}</button>
+        {move === currentMove ? "You are at move #" + move : <button onClick={() => setCurrentMove(move)}>{description}</button>}
       </li>
     )
   })
