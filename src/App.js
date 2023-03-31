8a16f36b722bb833f46871476c61c284347cf934
shellyscheng <shellysc13@gmail.com>
2023-03-30 22:20:20 -0700

    feat: highlight squares when someone wins or display draw message

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 
-function Square({value, onSquareClick}) {
+function Square({value, onSquareClick, isWinnerSquare}) {
 
   return (  
-    <button className="square" onClick={onSquareClick}>
+    <button className={`square ${isWinnerSquare ? `active` : ``}`} onClick={onSquareClick}>
       {value}
     </button>
   )
@@ -12,7 +12,7 @@ function Square({value, onSquareClick}) {
 export function Board({ xIsNext, squares, onPlay }) {  
 
   function handleClick(i) {
-    if(squares[i] || calculateWinner(squares)) {
+    if(squares[i] || calculateWinner(squares)[0]) {
       return;
     }
     const nextSquares = squares.slice();
@@ -20,8 +20,17 @@ export function Board({ xIsNext, squares, onPlay }) {
     onPlay(nextSquares)
   }
 
-  const winner = calculateWinner(squares);
-  let status = winner ? "Winner: " + winner : "Next player: " + (xIsNext ? "X" : "O")
+  const winner = calculateWinner(squares)[0];
+  const winnerSquares = calculateWinner(squares)[1] || [];
+  let status;
+
+  if (winner) {
+    status = "Winner: " + winner
+  } else if (squares.every(square => square != null)) {
+    status = "Draw"
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O")
+  }
 
   const boardSquares =  []
   for (let i = 0; i < 3; i++) {
@@ -29,7 +38,7 @@ export function Board({ xIsNext, squares, onPlay }) {
     for (let j = 0; j < 3; j++) {
       const index = i * 3 + j
       row.push(
-        <Square key={index} value={squares[index]} onSquareClick={() => handleClick(index)} />
+        <Square key={index} value={squares[index]} onSquareClick={() => handleClick(index)} isWinnerSquare={winnerSquares.includes(index) ? true : false}/>
       )
     }
     boardSquares.push(<div key={i} className="board-row">{row}</div>)
@@ -107,9 +116,9 @@ function calculateWinner(squares) {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a]
+      return [squares[a], lines[i]]
     }
   }
 
-  return null;
+  return [null, null];
 }
